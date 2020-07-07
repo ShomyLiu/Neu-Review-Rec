2f5a2718005f6c5e9b3eb5671c4808c0cc7d6a56
HTLiu <shomyliu@gmail.com>
2020-07-07 21:09:26 +0800

    add test/val/train data pth

--- dataset/data_review.py
+++ dataset/data_review.py
@@ -7,19 +7,23 @@ from torch.utils.data import Dataset
 
 class ReviewData(Dataset):
 
-    def __init__(self, root_path, train=True):
-        if train:
+    def __init__(self, root_path, mode):
+        if mode == 'Train':
             path = os.path.join(root_path, 'train/')
             print('loading train data')
             self.data = np.load(path + 'Train.npy', encoding='bytes')
             self.scores = np.load(path + 'Train_Score.npy')
+        elif mode == 'Val':
+            path = os.path.join(root_path, 'val/')
+            print('loading val data')
+            self.data = np.load(path + 'Val.npy', encoding='bytes')
+            self.scores = np.load(path + 'Val_Score.npy')
         else:
             path = os.path.join(root_path, 'test/')
             print('loading test data')
             self.data = np.load(path + 'Test.npy', encoding='bytes')
             self.scores = np.load(path + 'Test_Score.npy')
         self.x = list(zip(self.data, self.scores))
-        print('loading finish.')
 
     def __getitem__(self, idx):
         assert idx < len(self.x)
