2ace6c0d33e1f3f2422787bb296dfe52a03e762c
HTLiu <shomyliu@gmail.com>
2020-07-07 21:27:57 +0800

    divide test and validation, refine the print

--- main.py
+++ main.py
@@ -25,26 +25,6 @@ def collate_fn(batch):
     return data, label
 
 
-def unpack_input(opt, x):
-
-    uids, iids = list(zip(*x))
-    uids = list(uids)
-    iids = list(iids)
-
-    user_reviews = opt.users_review_list[uids]
-    user_item2id = opt.user2itemid_list[uids]  # 检索出该user对应的item id
-
-    user_doc = opt.user_doc[uids]
-
-    item_reviews = opt.items_review_list[iids]
-    item_user2id = opt.item2userid_list[iids]  # 检索出该item对应的user id
-    item_doc = opt.item_doc[iids]
-
-    data = [user_reviews, item_reviews, uids, iids, user_item2id, item_user2id, user_doc, item_doc]
-    data = list(map(lambda x: torch.LongTensor(x).cuda(), data))
-    return data
-
-
 def train(**kwargs):
 
     if 'dataset' not in kwargs:
@@ -68,15 +48,17 @@ def train(**kwargs):
         if len(opt.gpu_ids) > 0:
             model = nn.DataParallel(model, device_ids=opt.gpu_ids)
 
+    if model.net.num_fea != opt.num_fea:
+        raise ValueError(f"the num_fea of {opt.model} is error, please specific --num_fea={model.net.num_fea}")
+
     # 3 data
-    train_data = ReviewData(opt.data_root, train=True)
-    train_data_loader = DataLoader(train_data, batch_size=opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=collate_fn)
-    test_data = ReviewData(opt.data_root, train=False)
-    test_data_loader = DataLoader(test_data, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers, collate_fn=collate_fn)
-    print('{}: train data: {}; test data: {}'.format(now(), len(train_data), len(test_data)))
+    train_data = ReviewData(opt.data_root, mode="Train")
+    train_data_loader = DataLoader(train_data, batch_size=opt.batch_size, shuffle=True, collate_fn=collate_fn)
+    val_data = ReviewData(opt.data_root, mode="Val")
+    val_data_loader = DataLoader(val_data, batch_size=opt.batch_size, shuffle=False, collate_fn=collate_fn)
+    print(f'train data: {len(train_data)}; test data: {len(val_data)}')
 
     optimizer = optim.Adam(model.parameters(), lr=opt.lr, weight_decay=opt.weight_decay)
-
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.8)
 
     # training
@@ -91,13 +73,13 @@ def train(**kwargs):
         total_loss = 0.0
         total_maeloss = 0.0
         model.train()
-        print("{} Epoch {}: start".format(now(), epoch))
         for idx, (train_datas, scores) in enumerate(train_data_loader):
             if opt.use_gpu:
                 scores = torch.FloatTensor(scores).cuda()
             else:
                 scores = torch.FloatTensor(scores)
             train_datas = unpack_input(opt, train_datas)
+
             optimizer.zero_grad()
             output = model(train_datas)
             mse_loss = mse_func(output, scores)
@@ -105,9 +87,7 @@ def train(**kwargs):
 
             mae_loss = mae_func(output, scores)
             total_maeloss += mae_loss.item()
-
             smooth_mae_loss = smooth_mae_func(output, scores)
-
             if opt.loss_method == 'mse':
                 loss = mse_loss
             if opt.loss_method == 'rmse':
@@ -116,47 +96,77 @@ def train(**kwargs):
                 loss = mae_loss
             if opt.loss_method == 'smooth_mae':
                 loss = smooth_mae_loss
-
             loss.backward()
             optimizer.step()
-
             if opt.fine_step:
                 if idx % opt.print_step == 0 and idx > 0:
                     print("\t{}, {} step finised;".format(now(), idx))
-                    predict_loss, test_mse = predict(model, test_data_loader, opt, use_gpu=opt.use_gpu)
-                    if predict_loss < min_loss:
+                    val_loss, val_mse, val_mae = predict(model, val_data_loader, opt)
+                    if val_loss < min_loss:
                         model.save(name=opt.dataset, opt=opt.print_opt)
-                        min_loss = predict_loss
+                        min_loss = val_loss
                         print("\tmodel save")
-                    if predict_loss > min_loss:
+                    if val_loss > min_loss:
                         best_res = min_loss
 
-        scheduler.step(epoch)
-        print("{}; epoch:{}; total_loss:{}".format(now(), epoch, total_loss))
+        scheduler.step()
         mse = total_loss * 1.0 / len(train_data)
-        mae = total_maeloss * 1.0 / len(train_data)
-        print("{};train reslut: mse: {}; rmse: {}; mae: {}".format(now(), mse, math.sqrt(mse), mae))
+        print(f"{now()}; epoch:{epoch}...\n\ttrain data: loss:{total_loss:.4f}, mse: {mse:.4f};")
 
-        predict_loss, test_mse = predict(model, test_data_loader, opt, use_gpu=opt.use_gpu)
-        if predict_loss < min_loss:
+        val_loss, val_mse, val_mae = predict(model, val_data_loader, opt)
+        if val_loss < min_loss:
             model.save(name=opt.dataset, opt=opt.print_opt)
-            min_loss = predict_loss
+            min_loss = val_loss
             print("model save")
-        if test_mse < best_res:
-            best_res = test_mse
+        if val_mse < best_res:
+            best_res = val_mse
+        print("*"*30)
 
     print("----"*20)
     print(f"{now()} {opt.dataset} {opt.print_opt} best_res:  {best_res}")
     print("----"*20)
 
 
-def predict(model, test_data_loader, opt, use_gpu=True):
+def test(**kwargs):
+
+    if 'dataset' not in kwargs:
+        opt = getattr(config, 'Digital_Music_data_Config')()
+    else:
+        opt = getattr(config, kwargs['dataset'] + '_Config')()
+    opt.parse(kwargs)
+    assert(len(opt.pth_path) > 0)
+    random.seed(opt.seed)
+    np.random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+    if opt.use_gpu:
+        torch.cuda.manual_seed_all(opt.seed)
+
+    if len(opt.gpu_ids) == 0 and opt.use_gpu:
+        torch.cuda.set_device(opt.gpu_id)
+
+    model = Model(opt, getattr(models, opt.model))
+    if opt.use_gpu:
+        model.cuda()
+        if len(opt.gpu_ids) > 0:
+            model = nn.DataParallel(model, device_ids=opt.gpu_ids)
+    if model.net.num_fea != opt.num_fea:
+        raise ValueError(f"the num_fea of {opt.model} is error, please specific --num_fea={model.net.num_fea}")
+
+    model.load(opt.pth_path)
+    print(f"load model: {opt.pth_path}")
+    test_data = ReviewData(opt.data_root, mode="Test")
+    test_data_loader = DataLoader(test_data, batch_size=opt.batch_size, shuffle=False, collate_fn=collate_fn)
+    print(f"{now()}: test in the test datset")
+    predict_loss, test_mse, test_mae = predict(model, test_data_loader, opt)
+
+
+def predict(model, data_loader, opt):
     total_loss = 0.0
     total_maeloss = 0.0
     model.eval()
     with torch.no_grad():
-        for idx, (test_data, scores) in enumerate(test_data_loader):
-            if use_gpu:
+        for idx, (test_data, scores) in enumerate(data_loader):
+            if opt.use_gpu:
                 scores = torch.FloatTensor(scores).cuda()
             else:
                 scores = torch.FloatTensor(scores)
@@ -169,12 +179,32 @@ def predict(model, test_data_loader, opt, use_gpu=True):
             mae_loss = torch.sum(abs(output-scores))
             total_maeloss += mae_loss.item()
 
-    data_len = len(test_data_loader.dataset)
+    data_len = len(data_loader.dataset)
     mse = total_loss * 1.0 / data_len
     mae = total_maeloss * 1.0 / data_len
-    print("\t{};test reslut: mse: {}; rmse: {}; mae: {}".format(now(), mse, math.sqrt(mse), mae))
+    print(f"\tevaluation reslut: mse: {mse:.4f}; rmse: {math.sqrt(mse):.4f}; mae: {mae:.4f};")
     model.train()
-    return total_loss, mse
+    return total_loss, mse, mae
+
+
+def unpack_input(opt, x):
+
+    uids, iids = list(zip(*x))
+    uids = list(uids)
+    iids = list(iids)
+
+    user_reviews = opt.users_review_list[uids]
+    user_item2id = opt.user2itemid_list[uids]  # 检索出该user对应的item id
+
+    user_doc = opt.user_doc[uids]
+
+    item_reviews = opt.items_review_list[iids]
+    item_user2id = opt.item2userid_list[iids]  # 检索出该item对应的user id
+    item_doc = opt.item_doc[iids]
+
+    data = [user_reviews, item_reviews, uids, iids, user_item2id, item_user2id, user_doc, item_doc]
+    data = list(map(lambda x: torch.LongTensor(x).cuda(), data))
+    return data
 
 
 if __name__ == "__main__":
